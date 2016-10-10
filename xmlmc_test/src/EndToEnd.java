a7cb40ced75107a4818bb66b938df59f7002595d
Rich Gordon <rgordon@bittercreektech.com>
2016-10-10 12:04:36 -0600

    Update documentation

--- xmlmc_test/src/EndToEnd.java
+++ xmlmc_test/src/EndToEnd.java
@@ -2,6 +2,7 @@
  * An end to end test for the java xmlmc api demonstrating how to import and use the compiled jar.
  * You'll need to make sure that the xmlmc jar file is in your classpath. Working without an IDE
  * the easiest way to do that is by running java -classpath .;<path to xmlmc.jar>
+ * in this repository, that can be found at xmlmc/out/artifacts/xmlmc_jar
  */
 
 import xmlmc.Response;
