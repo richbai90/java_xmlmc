c07bf34482228efe5411a39e79787c8da64d3bba
Rich Gordon <rgordon@bittercreektech.com>
2016-10-10 12:01:56 -0600

    Create an end to end test and remove some in process files that we decided not to implement.

---
+++ xmlmc_test/src/EndToEnd.java
@@ -0,0 +1,51 @@
+/**
+ * An end to end test for the java xmlmc api demonstrating how to import and use the compiled jar.
+ * You'll need to make sure that the xmlmc jar file is in your classpath. Working without an IDE
+ * the easiest way to do that is by running java -classpath .;<path to xmlmc.jar>
+ */
+
+import xmlmc.Response;
+import xmlmc.XmlMethodCall;
+import xmlmc.types.Call;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class EndToEnd {
+    public static void main(String[] args) {
+        try {
+            XmlMethodCall methodCall = new XmlMethodCall("192.168.1.138");
+            //Perform a logon attempt
+            Response sessionEstablished = methodCall.session().analystLogon("admin", "");
+
+            if (sessionEstablished.isSuccessful()) {
+
+                //Create a new call object
+
+                Call call = new Call(methodCall, "incident");
+                call.setDescription("this is a test call. Logged from the java api");
+                call.setCustomer("alanc");
+
+                //Sla Name, Impact, Urgency
+                call.setImpactAndUrgency("Service Delivery - Silver Service (10 Days)", "High", "High");
+
+                Response response = methodCall.helpdesk().logAndAcceptNewCall(call);
+
+                //Print the xml response
+                System.out.println(response);
+
+                //Print the callref value
+                System.out.println(response.getParameter("callref"));
+
+                //logoff
+                methodCall.session().analystLogoff();
+            } else {
+                System.out.println(sessionEstablished.getLastError());
+            }
+
+        } catch (IOException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
